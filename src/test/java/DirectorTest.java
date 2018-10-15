4fbe5e79b740219c32407a1fb860697e16020fa2
nimbus117 <mail@nimbus117.co.uk>
2018-10-15 23:12:43 +0100

    added Director class and test

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,50 @@
+import management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class DirectorTest {
+
+    Director director;
+    @Before
+    public void before() {
+        director = new Director("Bob", "Z0001JK", 100, "Managed Services", 500);
+    }
+
+    @Test
+    public void hasSalery() {
+        assertEquals(100.0, director.getSalary());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals("Z0001JK", director.getNiNumber());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Bob", director.getName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(20);
+        assertEquals(120.0, director.getSalary());
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(1.0, director.payBonus());
+    }
+
+    @Test
+    public void hasDeptName() {
+        assertEquals("Managed Services", director.getDeptName());
+    }
+
+    @Test
+    public void hasBudget() {
+        assertEquals(500.0, director.getBudget());
+    }
+}
