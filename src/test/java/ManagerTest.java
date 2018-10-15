6a4129099c5e3a7f435f648154bcf6763bb6180e
nimbus117 <mail@nimbus117.co.uk>
2018-10-15 22:31:35 +0100

    added Manager class and tests

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,45 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+    @Before
+    public void before() {
+        manager = new Manager("Bob", "Z0001JK", 100, "Managed Services");
+    }
+
+    @Test
+    public void hasSalery() {
+        assertEquals(100.0, manager.getSalary());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals("Z0001JK", manager.getNiNumber());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Bob", manager.getName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(20);
+        assertEquals(120.0, manager.getSalary());
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(1.0, manager.payBonus());
+    }
+
+    @Test
+    public void hasDeptName() {
+        assertEquals("Managed Services", manager.getDeptName());
+    }
+}
